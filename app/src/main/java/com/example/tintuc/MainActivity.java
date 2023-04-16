8c713e41cd96bd7a1c306cb5cbec5c29bd42ea8d
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2023-04-16 21:13:28 +0700

    create project app tin tuc

---
+++ app/src/main/java/com/example/tintuc/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.tintuc;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.tintuc.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/tintuc/ManageNewsAdminActivity.java
@@ -0,0 +1,9 @@
+package com.example.tintuc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageNewsAdminActivity extends AppCompatActivity {
+
+}
