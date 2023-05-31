f4cce4880c56c9aae1c965c1b6ee6c147271b588
Nguyen-Van-Truong <20130187@st.hcmuaf.edu.vn>
2023-05-31 07:45:49 +0700

    gop truong vao main

--- app/src/main/java/com/example/sharingapp/MainActivity.java
+++ app/src/main/java/com/example/sharingapp/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         btnTestPDF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, PDFListActivity.class);
+                Intent intent = new Intent(MainActivity.this, PDFViewActivity.class);
                 startActivity(intent);
             }
         });
