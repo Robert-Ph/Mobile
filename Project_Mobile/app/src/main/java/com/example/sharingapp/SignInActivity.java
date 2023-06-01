accffaf1226d2efc660e98ba53873d5362a22ac2
Do Chi Thanh <20130118@st.hcmuaf.edu.vn>
2023-06-01 14:14:24 +0700

    Done SignUp SignIn

--- Project_Mobile/app/src/main/java/com/example/sharingapp/SignInActivity.java
+++ Project_Mobile/app/src/main/java/com/example/sharingapp/SignInActivity.java
@@ -1,28 +1,108 @@
 package com.example.sharingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import com.example.sharingapp.Ultils;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity {
+    private EditText editTextLoginEmail, editTextLoginPwd;
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
+    private static final String TAG = "SignInActivity";
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+
+        editTextLoginEmail = findViewById(R.id.email_input);
+        editTextLoginPwd = findViewById(R.id.password_input);
+        progressBar = findViewById(R.id.progressBar);
+
+        authProfile = FirebaseAuth.getInstance();
+
+        //show hide pwd
+        ImageView imageViewShowHidePwd = findViewById(R.id.imageView_show_hide_pwd);
+        imageViewShowHidePwd.setImageResource(R.drawable.ic_hide_pwd);
+        imageViewShowHidePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editTextLoginPwd.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())) {
+                    //if password is visible then Hide it
+                    editTextLoginPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    //change icon
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_hide_pwd);
+                } else {
+                    editTextLoginPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_show_pwd);
+                }
+            }
+        });
+
+
+        Button btnSignIn = findViewById(R.id.btn_doneSignIn);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textEmail = editTextLoginEmail.getText().toString();
+                String textPwd = editTextLoginPwd.getText().toString();
+
+                if (TextUtils.isEmpty(textEmail)) {
+                    Toast.makeText(SignInActivity.this, "Please enter your Email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()) {
+                    Toast.makeText(SignInActivity.this, "Please re-enter your Email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Valid email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if (TextUtils.isEmpty(textPwd)) {
+                    Toast.makeText(SignInActivity.this, "Please enter your Password", Toast.LENGTH_SHORT).show();
+                    editTextLoginPwd.setError("Password is required");
+                    editTextLoginPwd.requestFocus();
+
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail, textPwd);
+
+                }
+            }
+        });
+
+
+//------------------------------------------------------------------
 
         Button btn_back = (Button) findViewById(R.id.btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
@@ -54,33 +134,104 @@ public class SignInActivity extends AppCompatActivity {
                 alertDialog.show();
             }
         });
-        Button btn_signIn = (Button) findViewById(R.id.btn_doneSignIn);
-//        new Ultils().intent_toastNotion(btn_signIn,"Login Successful, SignInActivity.this, MainActivity2.class");
-        btn_signIn.setOnClickListener(new View.OnClickListener() {
+
+//---------------------------------------------------------------------
+
+//        Button btn_signIn = (Button) findViewById(R.id.btn_doneSignIn);
+////        new Ultils().intent_toastNotion(btn_signIn,"Login Successful, SignInActivity.this, MainActivity2.class");
+//        btn_signIn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+
+
+    }
+
+    private void loginUser(String email, String pwd) {
+        authProfile.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                LayoutInflater inflater = getLayoutInflater();
-                View layout = inflater.inflate(R.layout.custom_toast_layout, (ViewGroup) findViewById(R.id.custom_toast_container));
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
 
-                TextView text = layout.findViewById(R.id.text_notion);
-                text.setText("Login Successful!!!");
 
-                Toast toast = new Toast(getApplicationContext());
-                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-                toast.setDuration(Toast.LENGTH_SHORT);
-                toast.setView(layout);
-                toast.show();
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(SignInActivity.this, MainActivity2.class);
-                        startActivity(intent);
+                    //Get intanceof the current User
+                    FirebaseUser firebaseUser = authProfile.getCurrentUser();
+                    //check if email y verified before user can access their profile
+                    if (firebaseUser.isEmailVerified()) {
+                        Toast.makeText(SignInActivity.this, "You are logged in now", Toast.LENGTH_SHORT).show();
+                        //Open user Profile
+
+                    } else {
+                        firebaseUser.sendEmailVerification();
+                        authProfile.signOut();
+                        showAlertDialog();
+
                     }
-                }, 2000 + 1000);
+                    //Hien thi login successful
+                    LayoutInflater inflater = getLayoutInflater();
+                    View layout = inflater.inflate(R.layout.custom_toast_layout, (ViewGroup) findViewById(R.id.custom_toast_container));
+
+                    TextView text = layout.findViewById(R.id.text_notion);
+                    text.setText("Login Successful!!!");
+
+                    Toast toast = new Toast(getApplicationContext());
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.setDuration(Toast.LENGTH_SHORT);
+                    toast.setView(layout);
+                    toast.show();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intent = new Intent(SignInActivity.this, MainActivity2.class);
+                            startActivity(intent);
+                        }
+                    }, 2000 + 1000);
+
+
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthInvalidUserException e) {
+                        editTextLoginEmail.setError("User doesn't exists or is no longer valid. Please register again.");
+                        editTextLoginEmail.requestFocus();
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        editTextLoginEmail.setError("Invalid credentials. Kindly, check  and re-enter.");
+                        editTextLoginEmail.requestFocus();
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(SignInActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void showAlertDialog() {
+        //setup the Alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(SignInActivity.this);
+        builder.setTitle("Email not verified");
+        builder.setMessage("Please verify your email now. You can not login without email verification ");
+
+        //OPen email app  if user clicks/taps Continue button
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //to email app in new window and not within our app
+                startActivity(intent);
+
             }
         });
 
+        //Create the AlertDialog
+        AlertDialog alertDialog = builder.create();
 
+        //show the AlertDialog
+        alertDialog.show();
     }
 
 }
